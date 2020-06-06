6a47eeb40e375aa437f3938eb6dd4825dafd071b
erassmirza <erass.mirza@gmail.com>
2020-06-07 01:28:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -58,5 +58,21 @@ alert(course);
 alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
 
 // 6. Declare a variable called email and assign to it a string that represents
-//    your Email Address(e.g. example@example.com). Show the blow mentioned
+//    your Email Address(e.g. example@example.com). Show the below mentioned
 //    message in an alert box.(Hint: use string concatenation)
+var email = "erass.mirza@gmail.com";
+alert("My email address is "+email);
+
+// 7. Declare a variable called book & give it the value “A smarter way to 
+//    learn JavaScript”. Display the following message in an alert box
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book "+book);
+
+// 8. Write a script to display this in browser through JS
+document.write("Yah! I can write HTML content through JavaScript","<br></br>");
+
+// 9. Store following string in a variable and show in alert and browser 
+//    through JS
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
