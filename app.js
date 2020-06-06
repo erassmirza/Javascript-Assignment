6a2903652581251bf7c8883b46fc5394190948bb
erassmirza <erass.mirza@gmail.com>
2020-06-06 21:07:51 +0500

    First two chapters

---
+++ app.js
@@ -0,0 +1,62 @@
+// =========================> Chapter 01 <=========================
+
+// 1. Write a script to greet your website visitor using JS alert box.
+alert("Hello, Welcome to Javascript!");
+
+// 2. Write a script to display following message on your web page
+alert("Error! Please enter a valid password.");
+
+// 3. Write a script to display following message on your web page: 
+//    (Hint : Use line break)
+alert("Welcome to Js Land...\nHappy Coading!");
+
+// 4. Write a script to display following messages in sequence
+alert("Welcome to Js Land...");
+
+// 5. Generate the following message through browser’s developer console
+//  => Open this webpage and press ctrl+shift+i and go to console mode then type
+//     alert("Hello... I can run JS through my web browser's console")
+
+// 6. Make use of alerts in your new/existing HTML & CSS project
+
+// 7. Practice placement of <script></script> element in following sections of
+//    your project in exercise 6:
+//          a. Head
+//          b. Body (before your page’s HTML)
+//          c. Body (inside your page’s HTML)
+//          d. Body (after your page’s HTML
+
+
+
+// =========================> Chapter 02 <=========================
+
+// 1. Declare a variable called username
+var username;
+
+// 2. Declare a variable called myName & assign to it a string that represents
+//    your Full Name.
+var myName = "Erass Mirza";
+
+// 3. Write script to
+//      a) Declare a JS variable, titled message.
+//      b) Assign “Hello World” to variable message
+//      c) Display the message in alert box.
+var message;
+var message = "Hello World";
+alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and show the
+//    data in alert boxes
+var name = "Jhone Doe";
+var age = "15 years old";
+var course = "Certified Mobile Application Development";
+alert(name);
+alert(age);
+alert(course);
+
+// 5. Write a script to display the following alert using one JS variable
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// 6. Declare a variable called email and assign to it a string that represents
+//    your Email Address(e.g. example@example.com). Show the blow mentioned
+//    message in an alert box.(Hint: use string concatenation)
