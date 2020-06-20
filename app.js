e677d66383ed8804d37eda85751422ea47da758b
erassmirza <erass.mirza@gmail.com>
2020-06-20 23:37:52 +0500

    Chapter 12-13 update

--- app.js
+++ app.js
@@ -679,7 +679,21 @@ document.write("<hr />");
 
 // 1. Write a program that takes a character (number or string) in a variable & checks whether the given 
 //    input is a number, uppercase letter or lower case letter. (Hint: ASCII codes:- A=65, Z=90, a=97, z=122).
-
+var charTest = prompt("Enter value");
+var index = 0;
+if (charTest.charCodeAt(index)>=65 && charTest.charCodeAt(index)<=90) {
+    document.write("Your input character is Uppercase");
+}
+else if (charTest.charCodeAt(index)>=97 && charTest.charCodeAt(index)<=122) {
+    document.write("Your input character is Lowercase");
+}
+else if (charTest.charCodeAt(index)>=48 && charTest.charCodeAt(index)<=57) {
+    document.write("Your input is number");
+}
+else {
+    document.write("Invalid")
+}
+document.write("<hr />");
 
 // 2. Write a JavaScript program that accept two integers and display the larger. Also show if the two 
 //    integers are equal.
